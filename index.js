7f3ae7a10ec2f4620a1993c3964be4e26bee6705
Carl Whittaker <carl359@gmail.com>
2014-04-11 10:37:41 +0100

    ASCII bars

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ var PAGE = process.argv[2],
     '--start-maximized',
     '--remote-debugging-port=9222'
   ],
+  charm = require('charm')(),
   chrome = spawn(chromeExecutable, args);
+
+charm.pipe(process.stdout);
+charm.reset();
 
 function poll () {
   request('http://localhost:9222/json', function (err, res, body) {
@@ -29,9 +33,12 @@ function poll () {
 }
 
 function listen (tabs) {
-  var socketUrl = _.find(tabs, { url: PAGE }).webSocketDebuggerUrl;
+  var heapSize = 0,
+      lastHeap = 0,
+      i = 0,
+      socketUrl = _.find(tabs, { url: PAGE }).webSocketDebuggerUrl,
+      socket = new Socket(socketUrl);
 
-  var socket = new Socket(socketUrl);
   socket.onmessage = function (message) {
     var data = JSON.parse(message.data);
     if (data.params && data.params.record && data.params.record.usedHeapSizeDelta) {
@@ -41,10 +48,36 @@ function listen (tabs) {
   socket.on('open', function () {
     socket.send(JSON.stringify({ id: 1, method: 'Timeline.start' }));
   });
+
+  function logHeap (delta) {
+    heapSize += delta;
+  }
+
+  function write () {
+    if (i > 50) {
+      i = 0;
+      charm.reset();
+    }
+    charm.position(0, ++i);
+    var cols = Math.round((50 / 25e6) * heapSize);
+    var colour = 'yellow';
+    if (lastHeap > heapSize) colour = 'green';
+    if (lastHeap < heapSize) colour = 'red';
+    charm.background(colour);
+    label = heapSize + ' (' + (heapSize - lastHeap) + ')';
+    label = pad(label, cols + 20);
+    charm.write(label);
+    lastHeap = heapSize;
+  }
+
+  setInterval(write, 500);
 }
 
-function logHeap (delta) {
-  console.log(delta);
+function pad (str, length) {
+  var add = length - str.length;
+  add = add > 0 ? add : 0;
+  return str + (new Array(add)).join(' ');
 }
+
 
 poll();
